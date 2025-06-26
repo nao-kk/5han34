22c6008c6d67b725cc9c24e1515bf9dbf426acc6
sain1125 <mhsain1125@icloud.com>
2025-06-26 15:36:12 +0900

    UI製作途中

--- Assets/script/Timer_U.cs
+++ Assets/script/Timer_U.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script3 : MonoBehaviour
+public class Timer_U: MonoBehaviour
+   /* using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;*/
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
